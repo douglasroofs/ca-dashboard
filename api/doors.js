d99f5a90b0d7b1af77da75806c57ed9bd9338ef4
douglasroofs <kyle@douglasroofs.com>
2026-06-17 20:20:37 -0400

    Update doors.js

--- api/doors.js
+++ api/doors.js
@@ -34,7 +34,7 @@ const SNAPSHOTS = {
     allowedReps: ['steven arevalo', 'marc mitchell', 'andrew funk', 'michael mccarthy', 'george bechara', 'isabelle price', 'jack obert', 'harvey shoemaker', 'kevin mahan', 'robert wilson', 'andrew prickel', 'alfred duncan', 'christian brown', 'kelly alston', 'david kerns', 'aiden glonek', 'solomon lincoln jr.'],
     roster: ['Steven Arevalo', 'Marc Mitchell', 'Andrew Funk', 'Mike Mccarthy', 'George Bechara', 'Izzy Price', 'Jack Obert', 'Harvey Shoemaker', 'Kevin Mahan', 'Robert Wilson', 'Andrew Prickel', 'Alfred Duncan', 'Christian Brown', 'Kelly Alston', 'David Kerns', 'Aiden Glonek', 'Solomon Lincoln Jr.'],
   },
-  richmond: { updated: null, total: 0, reps: [], allowedReps: [], roster: [] },
+  richmond: {"updated":"2026-06-18T00:20:29.935Z","total":1464,"reps":[{"rep":"Andrew Harris","doors":355},{"rep":"Carter Massengill","doors":230},{"rep":"JR Zaguehi","doors":159},{"rep":"Travis Kizzar","doors":155},{"rep":"Logan Burbic","doors":146},{"rep":"Felipe Osorio","doors":137},{"rep":"Pedro Ramirez","doors":100},{"rep":"Joshua Baca","doors":75},{"rep":"Brandon Simmons","doors":66},{"rep":"Kenny Gonzalez","doors":41}],"allowedReps":["justin coghill","brandon simmons","travis kizzar","kevin mccann","joshua baca","logan burbic","bryan courtney","carter massengill","pedro ramirez","andrew harris","jr zaguehi","kenny gonzalez","dalton barr","cristina saunders","marcus schanewolf","felipe osorio"],"roster":["Justin Coghill","Brandon Simmons","Travis Kizzar","Kevin Mccann","Joshua Baca","Logan Burbic","Bryan Courtney","Carter Massengill","Pedro Ramirez","Andrew Harris","JR Zaguehi","Kenny Gonzalez","Dalton Barr","Cristina Saunders","marcus schanewolf","Felipe Osorio"]},
 };
 
 const BASE = 'https://api.salesrabbit.com';
